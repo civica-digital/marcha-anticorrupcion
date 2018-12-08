040d14022a4595a3a521efc3a17e002472e3366f
Ricardo Alanis <ricardo.alanis@gmail.com>
2018-12-08 13:53:15 -0600

    data

--- app.py
+++ app.py
@@ -123,4 +123,4 @@ def analyze_context():
     return jsonify(output)
 
 if __name__ == '__main__':
-    app.run(debug=True, ip="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
