521f527424db51287ebd18f5837226b58b62d438
Ricardo Alanis <ricardo.alanis@gmail.com>
2018-12-08 13:49:21 -0600

    Data

--- app.py
+++ app.py
@@ -123,4 +123,4 @@ def anticorruption_analysis():
     return app.response_class( response=json.dumps(output), mimetype='application/json' )
 
 if __name__ == '__main__':
-    app.run(debug=True, ip=0.0.0.0, port=5000)
+    app.run(debug=True, ip="0.0.0.0", port=5000)
