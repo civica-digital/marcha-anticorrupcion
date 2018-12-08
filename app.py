20f08e6f30a4b9d566cec305aed449a72fd9f138
Ubuntu <ricalanis@DataScience.p1vnx0bnzxwupdr014hu4ue5ed.bx.internal.cloudapp.net>
2018-12-08 23:44:19 +0000

    Add data

--- app.py
+++ app.py
@@ -61,63 +61,67 @@ def analyze_context():
         "probabilidad":0.91,
         "Soborno": {
             "probabilidad": 0.89,
-            "indicador_1": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad", "porcentaje":0.15}},
-            "indicador_2": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad", "porcentaje":0.35}},
-            "indicador_3": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad", "porcentaje":0.13}}
+            "indicador_1": {"tipo":"kpi", "datos":{"valor":20, "unidades":"unidad", "porcentaje":0.15}},
+            "indicador_2": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad", "porcentaje":1.00}}
         },
         "Desvio de Recursos": {
             "probabilidad": 0.25,
-            "indicador_1": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad", "porcentaje":0.4}},
-            "indicador_2": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad", "porcentaje":0.4}},
-            "indicador_3": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad", "porcentaje":0.4}}
+            "indicador_1": {"tipo":"kpi", "datos":{"valor":80, "unidades":"unidad", "porcentaje":0.4}},
+            "indicador_2": {"tipo":"kpi", "datos":{"valor":200, "unidades":"unidad", "porcentaje":1.0}},
+            "indicador_3": {"tipo":"kpi", "datos":{"valor":120, "unidades":"unidad", "porcentaje":0.6}}
         },
         "Abuso de funciones": {
             "probabilidad": 0.85,
-            "indicador_1": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad", "porcentaje":0.53}},
-            "indicador_2": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad", "porcentaje":0.13}},
-            "indicador_3": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad", "porcentaje":0.51}}
+            "indicador_1": {"tipo":"kpi", "datos":{"valor":100, "unidades":"unidad", "porcentaje":1.00}},
+            "indicador_2": {"tipo":"kpi", "datos":{"valor":34, "unidades":"unidad", "porcentaje":0.33}},
+            "indicador_3": {"tipo":"kpi", "datos":{"valor":67, "unidades":"unidad", "porcentaje":0.66}},
+            "indicador_4": {"tipo":"kpi", "datos":{"valor":67, "unidades":"unidad", "porcentaje":0.66}}
         },
         "Colusión": {
             "probabilidad": 0.51,
-            "indicador_1": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad", "porcentaje":0.21}},
-            "indicador_2": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad", "porcentaje":0.5}},
-            "indicador_3": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad", "porcentaje":0.8}}
+            "indicador_1": {"tipo":"kpi", "datos":{"valor":250, "unidades":"unidad", "porcentaje":1.00}},
+            "indicador_2": {"tipo":"kpi", "datos":{"valor":200, "unidades":"unidad", "porcentaje":0.8}},
+            "indicador_3": {"tipo":"kpi", "datos":{"valor":125, "unidades":"unidad", "porcentaje":0.5}},
+            "indicador_4": {"tipo":"kpi", "datos":{"valor":75, "unidades":"unidad", "porcentaje":0.3}},
+            "indicador_5": {"tipo":"kpi", "datos":{"valor":175, "unidades":"unidad", "porcentaje":0.7}}
         },
         "Conspiración para cometer actos de corrupción": {
             "probabilidad": 0.23,
-            "indicador_1": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad"}},
-            "indicador_2": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad"}},
-            "indicador_3": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad"}}
+            "indicador_1": {"tipo":"kpi", "datos":{"valor":150, "unidades":"unidad", "porcentaje":1.00}},
+            "indicador_2": {"tipo":"kpi", "datos":{"valor":61, "unidades":"unidad", "porcentaje":0.41}},
+            "indicador_3": {"tipo":"kpi", "datos":{"valor":20, "unidades":"unidad", "porcentaje":0.13}}
         },
         "Tráfico de influencia": {
             "probabilidad": 0.60,
-            "indicador_1": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad"}},
-            "indicador_2": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad"}},
-            "indicador_3": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad"}}
+            "indicador_1": {"tipo":"kpi", "datos":{"valor":1000, "unidades":"unidad", "porcentaje":1.00}},
+            "indicador_2": {"tipo":"kpi", "datos":{"valor":600, "unidades":"unidad", "porcentaje":0.60}},
         },
         "Enriquecimiento Oculto": {
             "probabilidad": 0.90,
-            "indicador_1": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad"}},
-            "indicador_2": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad"}},
-            "indicador_3": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad"}}
+            "indicador_1": {"tipo":"kpi", "datos":{"valor":800, "unidades":"unidad", "porcentaje":1.00}},
+            "indicador_2": {"tipo":"kpi", "datos":{"valor":430, "unidades":"unidad", "porcentaje":0.54}},
+            "indicador_3": {"tipo":"kpi", "datos":{"valor":240, "unidades":"unidad", "porcentaje":0.3}}
         },
         "Obstrucción de Justicia": {
             "probabilidad": 0.95,
-            "indicador_1": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad"}},
-            "indicador_2": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad"}},
-            "indicador_3": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad"}}
+            "indicador_1": {"tipo":"kpi", "datos":{"valor":130, "unidades":"unidad", "porcentaje":1.00}},
+            "indicador_2": {"tipo":"kpi", "datos":{"valor":96, "unidades":"unidad", "porcentaje":0.74}},
+            "indicador_3": {"tipo":"kpi", "datos":{"valor":58, "unidades":"unidad", "porcentaje":0.45}}
         },
         "Uso ilegal de información falsa o confidencial": {
             "probabilidad": 0.91,
-            "indicador_1": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad"}},
-            "indicador_2": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad"}},
-            "indicador_3": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad"}}
+            "indicador_1": {"tipo":"kpi", "datos":{"valor":250, "unidades":"unidad", "porcentaje":1.00}},
+            "indicador_2": {"tipo":"kpi", "datos":{"valor":200, "unidades":"unidad", "porcentaje":0.8}},
+            "indicador_3": {"tipo":"kpi", "datos":{"valor":125, "unidades":"unidad", "porcentaje":0.5}},
+            "indicador_4": {"tipo":"kpi", "datos":{"valor":75, "unidades":"unidad", "porcentaje":0.3}},
+            "indicador_5": {"tipo":"kpi", "datos":{"valor":175, "unidades":"unidad", "porcentaje":0.7}}
         },
         "Nepotismo": {
             "probabilidad": 0.85,
-            "indicador_1": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad"}},
-            "indicador_2": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad"}},
-            "indicador_3": {"tipo":"kpi", "datos":{"valor":131, "unidades":"unidad"}}
+            "indicador_1": {"tipo":"kpi", "datos":{"valor":100, "unidades":"unidad", "porcentaje":1.00}},
+            "indicador_2": {"tipo":"kpi", "datos":{"valor":34, "unidades":"unidad", "porcentaje":0.33}},
+            "indicador_3": {"tipo":"kpi", "datos":{"valor":67, "unidades":"unidad", "porcentaje":0.66}},
+            "indicador_4": {"tipo":"kpi", "datos":{"valor":67, "unidades":"unidad", "porcentaje":0.66}}
         },
     }
     return jsonify(output)
